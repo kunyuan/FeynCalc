29efa4e49ca0b29c818ad6640824d1c7e16c35e1
chenkun0228@gmail.com <kunchen@rusty1.cm.cluster>
2021-06-06 10:20:54 -0400

    fix bug in fock_static.jl

---
+++ submit_ccq.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+mkdir data
+cp -r ./groups* ./data
+cp -r ./*.exe ./data
+cp parameter ./data
+cp sigma.* ./data
+cd data
+
+sbatch -N1 -p gen -t 7-00:00:00 -J rs2b10l0.5 disBatch.py ../task
+sbatch -N1 -p gen -t 7-00:00:00 -J rs2b10l0.5 disBatch.py ../task
+#sbatch -N1 -p ccq -t 7-00:00:00 -J rs1 disBatch.py ../task
+#sbatch -N1 -p ccq -t 7-00:00:00 -J rs1 disBatch.py ../task
+#sbatch -n 28 -p gen -t 7-00:00:00 --ntasks-per-node 28 --exclusive --wrap="./feyncalc.exe"
+#sbatch -n 28 -p gen -t 7-00:00:00 --ntasks-per-node 28 --exclusive --wrap="disBatch.py ../task"
+#sbatch -a 0-28 -p ccq -t 7-00:00:00 -N1 --wrap="./feyncalc.exe"
+#sbatch -N1 -p genx -t 7-00:00:00 disBatch.py ../task
+
+
+#sbatch -N 1 -a 0-28 -p ccq -t 7-00:00:00 -n 1 --ntasks-per-node=28 --wrap="./feyncalc.exe"
+#sbatch -a 0-64 -p ccq -t 7-00:00:00 -n 1 --wrap="./feyncalc.exe"
