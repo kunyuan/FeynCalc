a84bc539af9fed2c928dc24eac8ca723812139fb
houpc <houpc96@gmail.com>
2022-02-22 01:00:39 +0800

    update

--- Polar_wn.jl
+++ Polar_wn.jl
@@ -23,7 +23,7 @@ const Qsize = 5
     NF::Float64 = kF / 2 / π^2
     β::Float64 = beta / EF
     qGrid::Vector{Float64}
-    nsize::Int = 11
+    nsize::Int = 20
     nGrid::Vector{Int64} = [n for n in 0:nsize-1]
     # μ = chemical_potential(beta) * EF
     χdlr = DLRGrid(EF * 100, beta / EF, 1e-8, false, :ph)
@@ -35,7 +35,7 @@ function readPara(fname)
     order, beta, rs, λ = param[1], param[2], param[3], param[5]
     minQ, maxQ = param[6], param[7]
     kF = (9π / 4)^(1 / 3) / rs
-    qGrid = [q for q in LinRange(0.0, maxQ - (maxQ - minQ) / Qsize, Qsize)]
+    qGrid = [q for q in LinRange(minQ, maxQ - (maxQ - minQ) / Qsize, Qsize)]
 
     return Para(beta = beta, rs = rs, λ = λ, order = order, kF = kF, qGrid = qGrid)
 end
@@ -48,7 +48,7 @@ function susceptibility(para, QBin)
     q = qGrid[QBin] * kF
     @printf("ExtMom: %f, DLR nsize: %d\n", q, χdlr.size)
 
-    filename = "Data_Polar/polar_beta$(beta)_rs$(rs)_lam$(λ)_o$(order)_q$(QBin-1).dat1"
+    filename = "Data_Polar/polar_beta$(beta)_rs$(rs)_lam$(λ)_o$(order)_q$(QBin-1).dat2"
     print("Loading ", filename, "\n")
     polar_tau = readdlm(filename)
     χ_tau::Vector{Float64} = polar_tau[6:end, 2]
@@ -73,7 +73,8 @@ function run()
     p = Gaston.Figure[]
     Fig = Gaston.Figure[]
     wnGrid = nGrid * 2π / β
-    for qi in 2:Qsize
+    for qi in 1:Qsize
+        println("ω_0 = $(2π/β),  vF*q = $(qGrid[qi]*EF*2)")
         χ0, χ = susceptibility(para, qi)
         # y0 = χ0 .* (wnGrid .* wnGrid) / (qGrid[qi] * kF)^2
         # y = χ .* (wnGrid .* wnGrid) / (qGrid[qi] * kF)^2
@@ -83,24 +84,30 @@ function run()
         f_xc = 1 ./ χ - 1 ./ χ0
         # @printf(f, "%d\t%10.8f\t%10.8f\t%10.8f\t%10.8f\t%10.8f\t%10.8f\n", n, wnGrid[idn], y[idn], y0[idn], χ[idn], χ0[idn], f_xc[idn])
 
+        x = wnGrid / EF
+        y = f_xc * NF
+        # y = f_xc * NF * (qGrid[qi] * kF)^2 ./ (wnGrid .* wnGrid)
         # Figmid =
-        if qi == 2
-            plot(wnGrid / EF, f_xc * NF, curveconf = "tit 'q=$(qGrid[qi]) kF' w lp lw 1 lc '#08F7FE'",
-                Axes(yrange = (-2, 3),
+        if qi == 1
+            plot(x, y, curveconf = "tit 'q=$(qGrid[qi]) kF' w lp lw 1 lc '#08F7FE'",
+                Axes(# xrange = (0, 0.8),
+                    # yrange = (-4, 1.2),
+                    yrange = (-0.2, -0.05),
                     ylabel = "'f_{xc} N_F'",
+                    # ylabel = "'f_{xc} N_F (q/ω_n)^2'",
                     xlabel = "'ω_n/E_F'",
                     key = "b l",
                     title = "'beta=$beta, r_s=$rs, λ=$λ'")
             )
         else
-            plot!(wnGrid / EF, f_xc * NF, curveconf = "tit 'q=$(qGrid[qi]) kF' w lp lw 1")
+            plot!(x, y, curveconf = "tit 'q=$(qGrid[qi]) kF' w lp lw 1")
         end
         # push!(Fig, Figmid)
-        # save(term = "pdf", output = "./Plot/fxc_beta$(beta)_rs$(rs)_q$qi.pdf", linewidth = 1)
+        # save(term = "pdf", output = "./Plot/fxc_beta$(beta)_rs$(rs)_q$(qi)_qm.pdf", linewidth = 1)
     end
     # plot([Fig[1] Fig[2]; Fig[3] Fig[4]])
-    # save(term = "pdf", output = "./Plot/fxc_beta$(beta)_rs$(rs).pdf", linewidth = 1)
-    save(term = "pdf", output = "./Plot/fxc_beta$(beta)_rs$(rs)_lam$λ.pdf", linewidth = 1)
+    save(term = "pdf", output = "./Plot/fxc_beta$(beta)_rs$(rs)_lam$(λ)_qm.pdf", linewidth = 1)
+    # save(term = "pdf", output = "./Plot/fxcRe_beta$(beta)_rs$(rs)_lam$λ.pdf", linewidth = 1)
 end
 
 run()
